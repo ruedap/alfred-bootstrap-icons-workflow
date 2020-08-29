337676e40fa0971a7af79f7482c03ad3792a580d
ruedap <ruedap@ruedap.com>
2020-08-29 17:08:28 +0900

    test(src): Fix icons_object.test.ts

--- src/assets/icons_object.test.ts
+++ src/assets/icons_object.test.ts
@@ -1,12 +1,33 @@
 import { getAllIconsObject } from "./icons_object";
 
-test("getAllIconsObject()", () => {
-  const obj = getAllIconsObject();
-  expect(obj.length).toBe(1120);
+describe("getAllIconsObject()", () => {
+  test("length", () => {
+    const obj = getAllIconsObject();
+    expect(obj.length).toBe(1120);
+  });
 
-  const actual = obj.find((icon) => icon.name === "bootstrap");
-  const expected = {
-    name: "bootstrap",
-  };
-  expect(actual).toStrictEqual(expected);
+  test("icon: bootstrap", () => {
+    const obj = getAllIconsObject();
+    const actual = obj.find((icon) => icon.name === "bootstrap");
+    const expected = {
+      name: "bootstrap",
+      title: "Bootstrap",
+      categories: ["Bootstrap"],
+      tags: ["bootstrap"],
+    };
+    expect(actual).toStrictEqual(expected);
+  });
+
+  test("icon: bag-plus", () => {
+    const obj = getAllIconsObject();
+    const actual = obj.find((icon) => icon.name === "bag-plus");
+    const expected = {
+      name: "bag-plus",
+      title: "Bag plus",
+      layout: "icon",
+      categories: ["Commerce"],
+      tags: ["shopping", "checkout", "check", "cart", "basket", "bag"],
+    };
+    expect(actual).toStrictEqual(expected);
+  });
 });
