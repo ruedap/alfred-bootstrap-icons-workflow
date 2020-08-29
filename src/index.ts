9e5ec98c61581712baf6d011d2f0e7ddd6b31691
ruedap <ruedap@ruedap.com>
2020-08-29 17:56:38 +0900

    fix(src): Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,37 @@
 import { getAllIconsObject } from "./assets/icons_object";
-import { putCode, putName, putRef, putUrl } from "./put";
+import { putName, putUrl } from "./put";
 import { getArgs, getFlagArgs, includesFlag } from "./query";
 import { search, toJson } from "./search";
 
-const FLAG_FIND = "--find";
-const FLAG_PUT_NAME = "--put-name";
-const FLAG_PUT_CODE = "--put-code";
-const FLAG_PUT_REF = "--put-ref";
-const FLAG_PUT_URL = "--put-url";
+const FLAG = {
+  FIND: "--find",
+  PUT: {
+    NAME: "--put-name",
+    URL: "--put-url",
+  },
+};
 
 const main = () => {
   const args = getArgs();
   const list = getAllIconsObject();
   const keys = ["name", "search.terms"];
 
-  if (includesFlag(args, FLAG_FIND)) {
-    const query = getFlagArgs(args, FLAG_FIND);
+  if (includesFlag(args, FLAG.FIND)) {
+    const query = getFlagArgs(args, FLAG.FIND);
     const searchResult = search(list, keys, query);
     console.log(toJson(searchResult)); // NOTE: Output to Alfred
     return;
   }
 
-  if (includesFlag(args, FLAG_PUT_NAME)) {
-    const query = getFlagArgs(args, FLAG_PUT_NAME);
+  if (includesFlag(args, FLAG.PUT.NAME)) {
+    const query = getFlagArgs(args, FLAG.PUT.NAME);
     const result = putName(query);
     result && console.log(result); // NOTE: Output to Alfred
     return;
   }
 
-  if (includesFlag(args, FLAG_PUT_CODE)) {
-    const query = getFlagArgs(args, FLAG_PUT_CODE);
-    const result = putCode(query);
-    result && console.log(result); // NOTE: Output to Alfred
-    return;
-  }
-
-  if (includesFlag(args, FLAG_PUT_REF)) {
-    const query = getFlagArgs(args, FLAG_PUT_REF);
-    const result = putRef(query);
-    result && console.log(result); // NOTE: Output to Alfred
-    return;
-  }
-
-  if (includesFlag(args, FLAG_PUT_URL)) {
-    const query = getFlagArgs(args, FLAG_PUT_URL);
+  if (includesFlag(args, FLAG.PUT.URL)) {
+    const query = getFlagArgs(args, FLAG.PUT.URL);
     const result = putUrl(query);
     result && console.log(result); // NOTE: Output to Alfred
     return;
