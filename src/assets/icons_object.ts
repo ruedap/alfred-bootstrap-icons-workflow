54451d5222aa6ebd2895198b1ddbad440536c3df
ruedap <ruedap@ruedap.com>
2020-08-29 16:06:34 +0900

    feat(src): Fix assets/icons_object.ts

--- src/assets/icons_object.ts
+++ src/assets/icons_object.ts
@@ -1,52 +1,9 @@
 import IconsJson from "@/assets/icons.json";
 
-type TIconJson = Readonly<{
-  changes: string[];
-  ligatures: string[];
-  search: {
-    terms: string[];
-  };
-  styles: string[];
-  unicode: string;
-  label: string;
-  voted?: boolean;
-  svg: {
-    [key: string]: {
-      last_modified: number;
-      raw: string;
-      viewBox: string[];
-      width: number;
-      height: number;
-      path: string;
-    };
-  };
-  free: string[];
-}>;
-
-type TIconsJson = Readonly<{
-  [key: string]: TIconJson;
-}>;
-
-export const getAllIconsJson = (): TIconsJson => {
-  return IconsJson as TIconsJson;
-};
-
-export type TIconObject = Pick<
-  TIconJson,
-  "search" | "unicode" | "label" | "free"
-> & {
+type TIconObject = Readonly<{
   name: string;
-};
+}>;
 
 export const getAllIconsObject = (): TIconObject[] => {
-  return Object.entries(IconsJson as TIconsJson).map(([key, value]) => {
-    return {
-      name: key,
-      label: value.label,
-      unicode: value.unicode,
-      search: value.search,
-      styles: value.styles,
-      free: value.free,
-    } as TIconObject;
-  });
+  return IconsJson as TIconObject[];
 };
