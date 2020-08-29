5ec5180892846d309fab861c20e334632a558162
ruedap <ruedap@ruedap.com>
2020-08-29 20:51:23 +0900

    test(assets): Add test for png files

---
+++ src/assets/icons/png_files.test.ts
@@ -0,0 +1,8 @@
+import fg from "fast-glob";
+
+describe("./assets/icons/*.png", () => {
+  test("length", () => {
+    const icons: string[] = fg.sync("./assets/icons/*.png");
+    expect(icons.length).toBe(1120);
+  });
+});
