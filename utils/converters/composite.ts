90d1e48ded2a9299ffea655f4ee6c51e50d617a9
ruedap <ruedap@ruedap.com>
2020-09-02 21:37:36 +0900

    chore(utils): Add minify method to comosite.ts

--- utils/converters/composite.ts
+++ utils/converters/composite.ts
@@ -1,24 +1,44 @@
 import fg from "fast-glob";
+import path from "path";
 
 // FIXME
 // eslint-disable-next-line
 const sharp = require("sharp");
+// eslint-disable-next-line
+const imagemin = require('imagemin');
+// eslint-disable-next-line
+const imageminPngquant = require('imagemin-pngquant');
 
 const outputDir = "./assets/icons";
 const outputPaths = fg.sync(`${outputDir}/*.png`);
 
 const composite = async (paths: string[]) => {
-  await Promise.all(
+  console.log("start composite");
+  return await Promise.all(
     paths.map(async (p) => {
       const buffer = await sharp(p)
         .flatten({ background: "#ffffff" })
         .toBuffer();
 
       await sharp(buffer).toFile(p, (err: Error, info: string) => {
+        if (!err) return;
         console.log({ err: err, info: info });
       });
     })
   );
+};
+
+const minify = async (paths: string[]) => {
+  console.log("start start");
+  const destination = path.parse(paths[0]).dir;
+  return await imagemin(paths, {
+    destination,
+    plugins: [
+      imageminPngquant({
+        quality: [0.3, 0.5],
+      }),
+    ],
+  });
 };
 
 const EXAMPLES = {
@@ -29,8 +49,16 @@ const EXAMPLES = {
   ],
 };
 
-const main = () => {
-  composite(EXAMPLES.outputPaths);
+const main = async () => {
+  const paths = EXAMPLES.outputPaths;
+
+  await composite(paths);
+  console.log("end composite");
+
+  await minify(paths);
+  console.log("end minify");
+
+  console.log("composite is complete");
 };
 
 main();
