018edcb408ce92b4762559bf59cbc7992ab172ea
ruedap <ruedap@ruedap.com>
2020-12-06 00:48:24 +0900

    chore(release): Fix tests

--- src/assets/icons_object.test.ts
+++ src/assets/icons_object.test.ts
@@ -3,7 +3,7 @@ import { getAllIconsObject } from "./icons_object";
 describe("getAllIconsObject()", () => {
   test("length", () => {
     const obj = getAllIconsObject();
-    expect(obj.length).toBe(1120);
+    expect(obj.length).toBe(1151);
   });
 
   test("icon: bootstrap", () => {
